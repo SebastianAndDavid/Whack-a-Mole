45319df783dca311dffe6ce00dc25d4cb55a3e92
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-06-14 11:45:51 -0700

    ft. added grid of 25 to Board.jsx

---
+++ src/Components/Board.jsx
@@ -0,0 +1,32 @@
+import "./Components.css";
+export default function Board() {
+  return (
+    <div className="grid-container">
+      <div className="position-0">0</div>
+      <div className="position-1">1</div>
+      <div className="position-2">2</div>
+      <div className="position-3">3</div>
+      <div className="position-4">4</div>
+      <div className="position-5">5</div>
+      <div className="position-6">6</div>
+      <div className="position-7">7</div>
+      <div className="position-8">8</div>
+      <div className="position-9">9</div>
+      <div className="position-10">10</div>
+      <div className="position-11">11</div>
+      <div className="position-12">12</div>
+      <div className="position-13">13</div>
+      <div className="position-14">14</div>
+      <div className="position-15">15</div>
+      <div className="position-16">16</div>
+      <div className="position-17">17</div>
+      <div className="position-18">18</div>
+      <div className="position-19">19</div>
+      <div className="position-20">20</div>
+      <div className="position-21">21</div>
+      <div className="position-22">22</div>
+      <div className="position-23">23</div>
+      <div className="position-24">24</div>
+    </div>
+  );
+}
