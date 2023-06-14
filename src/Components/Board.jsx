97775d6128b424b55de40deb526845fe5894eb99
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-06-14 12:57:34 -0700

    ft. added gridPosition component and logic to determine background color

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,32 +1,47 @@
+import { useState } from "react";
 import "./Components.css";
+import GridPosition from "./GridPosition";
 export default function Board() {
+  const [position, setPosition] = useState(1);
+
+  const [inLineStyle, setInLineStyle] = useState(null);
+
+  function createRandomPosition() {
+    setPosition(Math.floor(Math.random() * 25));
+  }
+
+  const positionArray = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25,
+  ];
+
+  function changeColor() {
+    setInLineStyle({ backgroundColor: "red" });
+    return inLineStyle;
+  }
+
   return (
-    <div className="grid-container">
-      <div className="position-0">0</div>
-      <div className="position-1">1</div>
-      <div className="position-2">2</div>
-      <div className="position-3">3</div>
-      <div className="position-4">4</div>
-      <div className="position-5">5</div>
-      <div className="position-6">6</div>
-      <div className="position-7">7</div>
-      <div className="position-8">8</div>
-      <div className="position-9">9</div>
-      <div className="position-10">10</div>
-      <div className="position-11">11</div>
-      <div className="position-12">12</div>
-      <div className="position-13">13</div>
-      <div className="position-14">14</div>
-      <div className="position-15">15</div>
-      <div className="position-16">16</div>
-      <div className="position-17">17</div>
-      <div className="position-18">18</div>
-      <div className="position-19">19</div>
-      <div className="position-20">20</div>
-      <div className="position-21">21</div>
-      <div className="position-22">22</div>
-      <div className="position-23">23</div>
-      <div className="position-24">24</div>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          createRandomPosition();
+          changeColor();
+        }}
+      >
+        Click
+      </button>
+      <div className="grid-container">
+        {positionArray.map((x, i) => {
+          // does the index match the randomPosition, then also send down inline styles to the GridPosition
+          console.log("i", i);
+          console.log("position", position);
+          return i === position ? (
+            <GridPosition key={x + i} />
+          ) : (
+            <GridPosition key={x + i} style={inLineStyle} />
+          );
+        })}
+      </div>
+    </>
   );
 }
